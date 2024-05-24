4c749305c6aaa9776e6fd0061c3837a086bd70f6
1046099511 <1046099511@qq.com>
2024-05-24 16:41:57 +0800

    feat: 增加两种匹配模式

--- src-utools/.utools/preload.js
+++ src-utools/.utools/preload.js
@@ -1,12 +1,39 @@
 const enter = require("./libs/args/enter");
 const search = require("./libs/args/search");
 const select = require("./libs/args/select");
+const { context } = require("./libs/shared/store");
+const path = require("path");
 
 window.exports = {
   "file-browser": {
     mode: "list",
     args: {
       enter,
+      search,
+      select,
+      placeholder: '输入`..`返回上一级文件夹，输入`\\`获取更多指令'
+    }
+  },
+  "match-path": {
+    mode: "list",
+    args: {
+      enter(action, setList) {
+        context.currentPath = path.dirname(action.payload)
+        return enter(action, setList)
+      },
+      search,
+      select,
+      placeholder: '输入`..`返回上一级文件夹，输入`\\`获取更多指令'
+    }
+  },
+  "match-file": {
+    mode: "list",
+    args: {
+
+      enter(action, setList) {
+        context.currentPath = path.dirname(action.payload[0].path)
+        return enter(action, setList)
+      },
       search,
       select,
       placeholder: '输入`..`返回上一级文件夹，输入`\\`获取更多指令'
